cefec1187778fbd77e18f94585786ba0d918ef9b
Andrea Cleland <andreaclelan@gmail.com>
2022-01-27 18:08:18 -0800

    added image files to assets, created drop down menus for climate and architecture

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // import functions and grab DOM elements
+const climateSelect = document.getElementById('climate-selector');
+const architectureSelect = document.getElementById('architecture-selector');
+const climateDiv = document.getElementById('climate');
+const architectureDiv = document.getElementById('architecture');
+
 
 // let state
+climateCount = 0;
+archCount = 0;
+let slogans = [];
 
 // set event listeners 
+
+
+climateSelect.addEventListener('change', () => {
+    const image = document.createElement('img');
+    image.src = 
+    climateSelect.appendChild()
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
